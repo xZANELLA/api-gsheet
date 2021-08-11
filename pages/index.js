bd57a6bd4c005d7ee13314af516b3e57a39cf057
LuizHenriqueH11 <>
2021-08-11 00:27:07 +0000

    page tempo

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ function Home() {
   return (<div>
       <h1>Home</h1>
       
-      <Link href="/sobre">
-      <a>Acessar página Sobre</a>
+      <Link href="/tempo">
+      <a>Acessar página tempo</a>
       </Link>
 
   </div>
