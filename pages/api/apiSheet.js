15afb5876f78974e918b2c7173f2946e1414e718
LuizHenriqueH11 <>
2021-08-30 19:15:21 +0000

    teste apiSheet

---
+++ pages/api/apiSheet.js
@@ -0,0 +1,22 @@
+
+function apiSheet(request, response) {
+
+  const { GoogleSpreadsheet } = require('google-spreadsheet');
+  const credenciais = require('./credenciais.json');
+
+  const getDoc = async () => {
+      const doc = new GoogleSpreadsheet('1EOx10mMHKiMFjLIRYe42BMRrZ5ZeoyWThr_FWHIiNF0');
+      
+      await doc.useServiceAccountAuth({
+          client_email: credenciais.client_email,
+          private_key: credenciais.private_key.replace(/\\n/g, '\n')
+      })
+      await doc.loadInfo();
+      return doc;
+  }
+      response.json({
+        name: doc.title;
+      });
+  });
+  
+}
