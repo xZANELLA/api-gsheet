938b1bb375e5dce12a38c40decc03642ff9e1ba1
LuizHenriqueH11 <>
2021-08-31 02:29:27 +0000

    teste

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -1,14 +1,19 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const credenciais = require('./credenciais.json');
 
-    export default function handler(req, res){
+const getDoc = async () => {
+    const doc = new GoogleSpreadsheet('1EOx10mMHKiMFjLIRYe42BMRrZ5ZeoyWThr_FWHIiNF0');
+    
+    await doc.useServiceAccountAuth({
+        client_email: credenciais.client_email,
+        private_key: credenciais.private_key.replace(/\\n/g, '\n')
+    })
+    let auxdoc = await doc.loadInfo();
+    return auxdoc;
+}
 
-      const doc = new GoogleSpreadsheet('1EOx10mMHKiMFjLIRYe42BMRrZ5ZeoyWThr_FWHIiNF0');
-      
-      await doc.useServiceAccountAuth({
-          client_email: credenciais.client_email,
-          private_key: credenciais.private_key.replace(/\\n/g, '\n')
-      })
-      await doc.loadInfo();
-      res.status(200).json(doc)
+    export default function handler(req, res){
+      let docc = await getDoc();
+
+      res.status(200).json({"doc": docc});
   };
