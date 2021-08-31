b2e11f0d46d44238a41698690385987ac8c6f13e
LuizHenriqueH11 <>
2021-08-31 00:27:35 +0000

    update api-gsheet

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -1,19 +1,21 @@
-function sheet(request, response){
-
   const { GoogleSpreadsheet } = require('google-spreadsheet');
   const credenciais = require('./credenciais.json');
 
-  const getDoc = async () => {
+  const getDoc = async (req, res) => {
       const doc = new GoogleSpreadsheet('1EOx10mMHKiMFjLIRYe42BMRrZ5ZeoyWThr_FWHIiNF0');
       
-      await doc.useServiceAccountAuth({
+      try{
+        await doc.useServiceAccountAuth({
           client_email: credenciais.client_email,
           private_key: credenciais.private_key.replace(/\\n/g, '\n')
       })
       await doc.loadInfo();
-      return response.json({tittle: doc.tittle});
+      res.status(200).json({doc.tittle})
+
+      } catch(err){
+        res.status(500).json({statusCode: 500, message: err.message})
+      }
+      
   }
 
-  }
-
-export default sheet;
+export default getDoc;
