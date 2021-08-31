5b2f885a57d3cb54710f91b267d1bb0ceb2533ce
LuizHenriqueH11 <>
2021-08-31 02:20:47 +0000

    test3

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -1,17 +1,14 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const credenciais = require('./credenciais.json');
 
-const getDoc = async () => {
-    const doc = new GoogleSpreadsheet('1EOx10mMHKiMFjLIRYe42BMRrZ5ZeoyWThr_FWHIiNF0');
-    
-    await doc.useServiceAccountAuth({
-        client_email: credenciais.client_email,
-        private_key: credenciais.private_key.replace(/\\n/g, '\n')
-    })
-    await doc.loadInfo();
-    return doc;
-}
-
     export default function handler(req, res){
-      res.status(200).json({nome: "teste se esse trem funciona mesmo"})
+
+      const doc = new GoogleSpreadsheet('1EOx10mMHKiMFjLIRYe42BMRrZ5ZeoyWThr_FWHIiNF0');
+      
+      await doc.useServiceAccountAuth({
+          client_email: credenciais.client_email,
+          private_key: credenciais.private_key.replace(/\\n/g, '\n')
+      })
+      await doc.loadInfo();
+      res.status(200).json(doc)
   };
